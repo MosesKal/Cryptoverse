54e7365d517a6ef8bc6d59d74b07ad81f369afda
MosesKal <mosesziongo@gmail.com>
2024-04-05 15:11:18 +0200

    feat(front): redux configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import App from "./App.jsx";
 
-import 'antd/dist/antd.css';
+import store from "./app/store.js";
+
+import "antd/dist/antd.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>
 );
