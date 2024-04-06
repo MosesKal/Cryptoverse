78b8dac16b8c701734a6f89fe2e139b4840e9d31
MosesKal <mosesziongo@gmail.com>
2024-04-06 18:38:24 +0200

    feat(front): fetching data with fetchBaseQuery

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,16 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query/react'; 
 
+import { cryptoApi } from '../services/cryptoApi';
 
-export default configureStore({
-    reducer : {},
+const store = configureStore({
+  reducer: {
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(cryptoApi.middleware), 
 });
+
+setupListeners(store); 
+
+export default store;
